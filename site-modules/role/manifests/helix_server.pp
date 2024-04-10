1b3d3adfd7d505d3ec0f272aa1ec717479622b99
Andrew Jones <andrewj100@gmail.com>
2024-04-10 14:18:28 +0100

    Update and rename webserver.pp to helix_server.pp

--- site-modules/role/manifests/helix_server.pp
+++ site-modules/role/manifests/helix_server.pp
@@ -1,7 +1,7 @@
 # This role would be made of all the profiles that need to be included to make a webserver work
 # All roles should include the base profile
-class role::webserver {
+class role::helix_server {
 
-  include profile::base
+  include profile::helix_profile
 
 }
